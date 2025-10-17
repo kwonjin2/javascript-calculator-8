c3bca0eed2d0a2d31ef8d1a554adf7e90b5fe7e3
최권진 <rnjswls060@naver.com>
2025-10-17 21:13:55 +0900

    feat(App): static 에러 메시지 정의

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import { Console } from '@woowacourse/mission-utils';
 
 class App {
+  static ERROR_MESSAGES = {
+    EMPTY_STRING:
+      '[ERROR] 커스텀 구분자가 없습니다. //와 \\n 사이에 커스텀 구분자를 입력해주세요.',
+    NUMBER_DELIMITER: '[ERROR] 커스텀 구분자를 숫자로 지정할 수 없습니다.',
+    NEGATIVE_NUMBER: '[ERROR] 음수는 입력할 수 없습니다.',
+    INVALID_INPUT: '[ERROR] 입력이 올바르지 않습니다.',
+  };
+
   async run() {
     const input = await Console.readLineAsync(
       '덧셈할 문자열을 입력해 주세요. \n'
