45657848ecbe3a9a0f64a47d812ac564109b4c77
최권진 <rnjswls060@naver.com>
2025-10-20 13:35:14 +0900

    refactor(App): 클래스 내부 메서드 및 스태틱 변수 순서 변경

--- src/App.js
+++ src/App.js
@@ -1,19 +1,6 @@
 import { Console } from '@woowacourse/mission-utils';
 
 class App {
-  static ERROR_MESSAGES = {
-    EMPTY_STRING:
-      '[ERROR] 커스텀 구분자가 없습니다. //와 \\n 사이에 커스텀 구분자를 입력해주세요.',
-    NUMBER_DELIMITER: '[ERROR] 커스텀 구분자를 숫자로 지정할 수 없습니다.',
-    NEGATIVE_NUMBER: '[ERROR] 음수는 입력할 수 없습니다.',
-    INVALID_INPUT: '[ERROR] 입력이 올바르지 않습니다.',
-  };
-
-  static MESSAGES = {
-    INPUT: '덧셈할 문자열을 입력해 주세요. \n',
-    OUTPUT: '결과 :',
-  };
-
   async run() {
     const input = await Console.readLineAsync(App.MESSAGES.INPUT);
     const result = this.calculateSumFromString(input);
@@ -39,6 +26,20 @@ class App {
     return this.sumNumbers(numbers);
   }
 
+  deleteDelimiter(value) {
+    let delimiter = /,|:/;
+    let parts = value;
+
+    const customDelimiter = this.checkCustomDelimiter(value);
+    if (customDelimiter) {
+      const escapeDelimiter = this.escapeRegExp(customDelimiter);
+      delimiter = new RegExp(escapeDelimiter);
+      parts = value.split('\\n')[1];
+    }
+
+    return parts.split(delimiter);
+  }
+
   checkCustomDelimiter(input) {
     if (input.startsWith('//')) {
       const customDelimiter = input.slice(2, input.indexOf('\\n'));
@@ -54,20 +55,6 @@ class App {
     return string.replace(/[$^*()+[\]|\\?]/g, '\\$&');
   }
 
-  deleteDelimiter(value) {
-    let delimiter = /,|:/;
-    let parts = value;
-
-    const customDelimiter = this.checkCustomDelimiter(value);
-    if (customDelimiter) {
-      const escapeDelimiter = this.escapeRegExp(customDelimiter);
-      delimiter = new RegExp(escapeDelimiter);
-      parts = value.split('\\n')[1];
-    }
-
-    return parts.split(delimiter);
-  }
-
   mapToNumbers(stringArray) {
     return stringArray.map((str) => Number(str));
   }
@@ -75,6 +62,19 @@ class App {
   sumNumbers(numberArray) {
     return numberArray.reduce((acc, cur) => acc + cur, 0);
   }
+
+  static ERROR_MESSAGES = {
+    EMPTY_STRING:
+      '[ERROR] 커스텀 구분자가 없습니다. //와 \\n 사이에 커스텀 구분자를 입력해주세요.',
+    NUMBER_DELIMITER: '[ERROR] 커스텀 구분자를 숫자로 지정할 수 없습니다.',
+    NEGATIVE_NUMBER: '[ERROR] 음수는 입력할 수 없습니다.',
+    INVALID_INPUT: '[ERROR] 입력이 올바르지 않습니다.',
+  };
+
+  static MESSAGES = {
+    INPUT: '덧셈할 문자열을 입력해 주세요. \n',
+    OUTPUT: '결과 :',
+  };
 }
 
 export default App;
